43a262db344496c0477b32d7e0733faf652d49d2
yanzhe919 <yanzhe919@gmail.com>
2020-12-11 22:04:50 +0800

    IU-2020.2.3 <Administrator@yz Create p3c.xml, IntelliLang.xml, project.default.xml, diff.xml, equalsHashCodeTemplates.xml, Default.xml, find.xml, File Header.java, checkstyle-idea.xml, keymap.xml, code.style.schemes.xml, KeyPromoterXSettings.xml, YZ.xml, Default.xml, lightEdit.xml, alibaba-toolkit-startUp.xml, encoding.xml, editor.codeinsight.xml, editor.xml, debugger.xml, updates.xml, baseRefactoring.xml, ide.general.xml, YZ.xml, git.xml, rainbow_brackets.xml, lombok-plugin.xml, GoogleStyle.xml, ActionScript File Header.as, xpath.xml, code.style.schemes, ui.lnf.xml, gradle.settings.xml, filetypes.xml, projectView.xml, colors.scheme.xml, AiXCoderConfig_local.xml, databaseDrivers.xml, templates.xml, laf.xml, parameter.hints.xml, jdk.table.xml, path.macros.xml, mavenRunHelper.xml, web-browsers.xml, wsl.distributions.xml, proxy.settings.xml, vcs.xml, codota.xml, databaseSettings.xml, grazie_global.xml, toStringTemplates.xml, mybatis.xml, usageView.xml, setterTemplates.xml, sonarlint.xml

---
+++ fileTemplates/includes/ActionScript File Header.as
@@ -0,0 +1,9 @@
+/**
+ * 项目名称：${PROJECT_NAME}
+ * TODO
+ * 
+ * @author YZ
+ * @since ${DATE} ${TIME}
+ * @version 1.0
+ */
+
---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,8 @@
+/**
+ * 项目名称：${PROJECT_NAME}
+ * @Description TODO
+ * 
+ * @author YZ
+ * @since ${DATE} ${TIME}
+ * @version 1.0
+ */
